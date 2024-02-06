4de8a52d69b1fd5c8c169b75b30cabf68a2a58ea
Ibrahim <ibrhm.altay.12@gmail.com>
2024-02-06 12:33:08 -0500

    SimpleCounter

---
+++ src/js/component/icons.js
@@ -0,0 +1,10 @@
+import fontawesome from '@fortawesome/fontawesome'
+
+
+import faClock from '@fortawesome/fontawesome-free-reqular/faClock'
+
+fontawesome.config ={
+    autoReplaceSvg :'nest'
+};
+
+fontawesome.library.add(faClock);
